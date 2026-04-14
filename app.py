2297644c5eea5eaa6acb3ddd524c3ca9621e43e7
chicken-eng <60812096+chicken-eng@users.noreply.github.com>
2026-04-14 19:49:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 import streamlit as st
 from vanna.remote import VannaDefault
-import vanna
 
-st.write("Vanna version:", vanna.__version__)
-
-api_key = st.secrets.get("VANNA_API_KEY")
-vn = VannaDefault(api_key=api_key, model='chinook-11')
-st.write("Training data:", vn.get_training_data())
+vn = VannaDefault(api_key=st.secrets.get("VANNA_API_KEY"), model='fsi')
+st.write(vn.get_training_data())
