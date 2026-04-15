d8897abd3ff4afb8c29847d48cb3a82d167029be
chicken-eng <60812096+chicken-eng@users.noreply.github.com>
2026-04-15 08:54:04 +0100

    Update vanna_calls.py

--- vanna_calls.py
+++ vanna_calls.py
@@ -23,7 +23,7 @@ def get_engine():
 @st.cache_resource
 def get_llm():
     return ChatGoogleGenerativeAI(
-        model="gemini-1.5-flash",
+        model="gemini-2.5-flash",
         google_api_key=st.secrets["GEMINI_API_KEY"],
         temperature=0
     )
